7c4c8b529700f6b10938b450ceea9df9c4721c5e
shimotsu <shimotsu13@gmail.com>
2023-09-22 17:42:11 +0900

    change lang attribute

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default async function RootLayout({ children }: Props) {
     limit: LIMIT,
   });
   return (
-    <html lang="ja">
+    <html lang="en">
       <body>
         <Header />
         <Nav tags={tags.contents} />
