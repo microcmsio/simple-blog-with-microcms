0c97d3f24aeb99145b3774f049e2221b0a8d1bd1
nakanohiroko <nakano@microcms.co.jp>
2026-08-12 11:29:31 +0900

    chore: Next.jsをv16にアップグレード、関連パッケージを更新

--- components/Article/index.tsx
+++ components/Article/index.tsx
@@ -35,24 +35,26 @@ export default function Article({ data }: Props) {
         )}
         <PublishedDate date={data.publishedAt || data.createdAt} />
       </div>
-      <picture>
-        <source
-          type="image/webp"
-          media="(max-width: 640px)"
-          srcSet={`${data.thumbnail?.url}?fm=webp&w=414 1x, ${data.thumbnail?.url}?fm=webp&w=414&dpr=2 2x`}
-        />
-        <source
-          type="image/webp"
-          srcSet={`${data.thumbnail?.url}?fm=webp&fit=crop&w=960&h=504 1x, ${data.thumbnail?.url}?fm=webp&fit=crop&w=960&h=504&dpr=2 2x`}
-        />
-        <img
-          src={data.thumbnail?.url}
-          alt=""
-          className={styles.thumbnail}
-          width={data.thumbnail?.width}
-          height={data.thumbnail?.height}
-        />
-      </picture>
+      {data.thumbnail && (
+        <picture>
+          <source
+            type="image/webp"
+            media="(max-width: 640px)"
+            srcSet={`${data.thumbnail.url}?fm=webp&w=414 1x, ${data.thumbnail.url}?fm=webp&w=414&dpr=2 2x`}
+          />
+          <source
+            type="image/webp"
+            srcSet={`${data.thumbnail.url}?fm=webp&fit=crop&w=960&h=504 1x, ${data.thumbnail.url}?fm=webp&fit=crop&w=960&h=504&dpr=2 2x`}
+          />
+          <img
+            src={data.thumbnail.url}
+            alt=""
+            className={styles.thumbnail}
+            width={data.thumbnail.width}
+            height={data.thumbnail.height}
+          />
+        </picture>
+      )}
       <div
         className={styles.content}
         dangerouslySetInnerHTML={
